c2772ab70be32d4ad2f77a7371c9611fac694d7c
Pratham Vidhani <prathamvidhani239@gmail.com>
2025-09-05 06:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (3.50%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -8,10 +8,10 @@ public:
          int high = n;
          
 
-        while (low<=high){
+        while (low<high){
             int mid = low+(high-low)/2;
             if(isBadVersion(mid)  ){
-                high =mid -1;
+                high =mid ;
             }
             else {
                 low=mid+1;
