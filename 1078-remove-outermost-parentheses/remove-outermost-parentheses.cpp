f1569662dece8066a26806b7e2acc1bd63d36fc5
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-02-21 20:23:07 +0530

    Time: 23 ms (5.71%) | Memory: 67.2 MB (5.35%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int level = 0;
+        string ans = "";
+
+        for(int i =0; i <s.length(); i++){
+            if(s[i] == '('){
+                if(level > 0){
+                    ans= ans +s[i];
+                }
+                level++;
+            }
+            else if(s[i] == ')'){
+               level--;  
+                if(level > 0) 
+                    ans = ans + s[i];
+               
+            }
+        }
+        return ans;
+        
+    }
+};
