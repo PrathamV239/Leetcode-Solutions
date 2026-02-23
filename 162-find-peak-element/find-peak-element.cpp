f45519c6fc80626613ad83491b899525aba9f2d8
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-02-23 23:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (30.28%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -3,33 +3,41 @@ public:
     int findPeakElement(vector<int>& nums) {
 
          int n = nums.size();
-        int low =0, high = n;
+        int low =0, high = n-1;
         
         for(int i = 0; i < n; i++){
+            
             while (low < high){
-
-                int mid =( high + low )/ 2;
-                if(high==low+2){
-                    if(nums[low]<nums[high-1]){
-                        return high-1;
-                    }
-                    else return low;
-                }
-                if(mid==n-1){
-                    return mid;
-                }
-                if(mid==0){
-                    return mid;
-                }
-
-                if(nums[mid] > nums[mid - 1] && nums [mid] > nums[mid +1])
-                    return mid;
-                else if (nums[mid] < nums[mid -1])
+                    int mid =( high + low )/ 2;
+                if(nums[mid] > nums[mid +1])
                     high = mid;
-                else if(nums[mid] < nums[mid + 1]) 
-                    low = mid+1;                   
+                else 
+                    low = mid+1;
+
+        //         int mid =( high + low )/ 2;
+        //         if(high==low+2){
+        //             if(nums[low]<nums[high-1]){
+        //                 return high-1;
+        //             }
+        //             else return low;
+        //         }
+        //         if(mid==n-1){
+        //             return mid;
+        //         }
+        //         if(mid==0){
+        //             return mid;
+        //         }
+
+        //         if(nums[mid] > nums[mid - 1] && nums [mid] > nums[mid +1])
+        //             return mid;
+        //         else if (nums[mid] < nums[mid -1])
+        //             high = mid;
+        //         else if(nums[mid] < nums[mid + 1]) 
+        //             low = mid+1;                   
+        //     }
+        // }
             }
         }
-        return -1;   
+        return low;   
     }
 };
