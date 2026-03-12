b8629d6728531e74553d1e4299dd0478652c9951
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-03-13 00:03:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (59.90%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int countParti(vector<int>& nums, int maxSum){
+        int n = nums.size();
+        int partitions = 1;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            if(sum + nums[i] <= maxSum){
+                sum = sum + nums[i];
+            }
+            else{ 
+                partitions++;
+                sum = nums[i];
+            }
+        }
+        return partitions;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(),0);
+        int ans = 1;
+        while(low <= high){
+            int mid = (low + high ) / 2;
+            int parti = countParti(nums, mid);
+
+            if(parti > k)
+                low = mid+1;
+            else {
+                ans = mid;
+                high = mid -1;
+            }
+        }
+        return ans;
+    }
+};
