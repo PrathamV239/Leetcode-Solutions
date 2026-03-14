4a07df031418a241f756829564520eb3ac9313d6
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-03-15 04:42:37 +0530

    Time: 34 ms (5.45%) | Memory: 39.4 MB (5.09%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,27 +1,30 @@
 class Solution {
-public: 
-    long long timetaken(vector<int> &nums, int h){
-        long long sum = 0;
-        for (int i =0; i < nums.size(); i++){
-            sum = sum + ceil((double)nums[i] / h);
+public:
+    long long bananaspeed (vector <int> nums, int m, int mmid){
+        long long sum =0;
+    
+        for(int i =0; i < nums.size(); i++){
+            sum = sum + ceil((double)nums[i] / mmid);
         }
         return sum;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
+        int low =1;
         int high = *max_element(piles.begin(), piles.end());
-        int ans = 1;
+
+        int ans = 0;
 
         while(low <= high){
             int mid = (low + high) /2;
-            long long res = timetaken(piles, mid);
-            if(res > h)
-                low = mid +1;
-            else {
+            long long res = bananaspeed(piles,h,mid);
+            if(res <= h){
                 ans = mid;
-                high = mid -1;  
-            }      
-        }        
-        return ans;
+                high = mid -1;
+            }
+            else    
+                low = mid +1;
+        } 
+        return ans ;       
     }
 };
