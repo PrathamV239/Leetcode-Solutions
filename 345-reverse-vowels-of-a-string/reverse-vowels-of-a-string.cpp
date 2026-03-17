db7e017b4e982901762c46037245276e58e51d21
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-03-18 04:34:24 +0530

    Time: 3 ms (35.74%) | Memory: 10.4 MB (51.42%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -5,41 +5,41 @@ public:
         return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
     }
 
-    // string reverseVowels(string s) {
+    string reverseVowels(string s) {
         
-    //     int i = 0;
-    //     int j = s.length() -1;
+        int i = 0;
+        int j = s.length() -1;
         
-    //     while(i < j){
-    //        if(!isvowel(s[i]))
-    //             i++;
-    //         else if(!isvowel(s[j]))
-    //             j--;
-    //          else{ 
-    //             swap(s[i],s[j]);
-    //             i++;
-    //             j--;
-    //          }
+        while(i < j){
+           if(!isvowel(s[i]))
+                i++;
+            else if(!isvowel(s[j]))
+                j--;
+             else{ 
+                swap(s[i],s[j]);
+                i++;
+                j--;
+             }
 
-    //     }
-    //     return s;
-    // }
-
-
-    string reverseVowels(string s){
-        stack<char> st;
-        int n = s.length();
-        for(int i =0; i < n; i++){
-            if(isvowel(s[i]))
-                st.push(s[i]);
-        }
-
-        for(int i=0; i < n; i++){
-            if(isvowel(s[i])){
-                s[i] = st.top();
-                st.pop();
-            }
         }
         return s;
     }
+
+
+    // string reverseVowels(string s){
+    //     stack<char> st;
+    //     int n = s.length();
+    //     for(int i =0; i < n; i++){
+    //         if(isvowel(s[i]))
+    //             st.push(s[i]);
+    //     }
+
+    //     for(int i=0; i < n; i++){
+    //         if(isvowel(s[i])){
+    //             s[i] = st.top();
+    //             st.pop();
+    //         }
+    //     }
+    //     return s;
+    // }
 };
