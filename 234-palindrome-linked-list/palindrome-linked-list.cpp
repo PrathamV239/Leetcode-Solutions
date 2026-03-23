71fa7bf5d5a9c1e4497a0b3790b3fa350de858f3
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-03-24 04:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 124.2 MB (37.51%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,28 +10,69 @@
  */
 class Solution {
 public:
+    ListNode* reverseLL(ListNode* head){
+        if(head == nullptr || head-> next == nullptr)
+            return head;
+        ListNode* newhead = reverseLL(head -> next);
+        ListNode* front = head-> next;
+        front -> next = head;
+        head -> next = nullptr;
+        return newhead;
+    }
+
     bool isPalindrome(ListNode* head) {
-        ListNode* curr1 = head;
-        ListNode* curr2 = head;
-        stack <int> st;
-        if(head == nullptr || head ->next == nullptr)
-            return true;
+    
+    // O(N) TC
+    // O(N) SC
 
-        while(curr2 != nullptr && curr2->next != nullptr){            
-            st.push(curr1->val);
-            curr1 = curr1->next;
-            curr2 = curr2->next->next;
-        }
-        if(curr2 != nullptr && curr2->next == nullptr)
-            curr1 = curr1->next;
-        while (curr1 != nullptr && st.top() == curr1->val){
-            st.pop();
-            curr1= curr1->next;
-        }
+    //     ListNode* curr1 = head;
+    //     ListNode* curr2 = head;
+    //     stack <int> st;
+    //     if(head == nullptr || head ->next == nullptr)
+    //         return true;
+
+    //     while(curr2 != nullptr && curr2->next != nullptr){            
+    //         st.push(curr1->val);
+    //         curr1 = curr1->next;
+    //         curr2 = curr2->next->next;
+    //     }
+    //     if(curr2 != nullptr && curr2->next == nullptr)
+    //         curr1 = curr1->next;
+    //     while (curr1 != nullptr && st.top() == curr1->val){
+    //         st.pop();
+    //         curr1= curr1->next;
+    //     }
       
-        if(!st.empty())
-            return false;          
-    return true;
+    //     if(!st.empty())
+    //         return false;          
+    // return true;
 
+    if(head == nullptr || head -> next == nullptr )
+        return true;
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+
+    while(fast != nullptr && fast-> next != nullptr){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    ListNode* secondhalf = slow -> next;
+    slow -> next = nullptr;
+    ListNode* newhead = reverseLL(secondhalf);
+
+    ListNode* first = head;
+    ListNode* second = newhead;
+    while(second != nullptr){
+        if(first -> val != second-> val){
+         newhead = reverseLL(newhead);
+            slow ->next = newhead;
+            return false;
+        }
+        first = first -> next;
+        second = second -> next;
+    }
+    newhead = reverseLL(newhead);
+    slow ->next = newhead;
+    return true;
     }
 };
