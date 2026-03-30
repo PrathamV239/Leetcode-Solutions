d35362eaa633e078c349a89d5a67650e88fa719d
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-03-30 10:45:28 +0530

    Time: 1615 ms (5.01%) | Memory: 52.4 MB (56.81%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       int maxInd =0;
+       int n = nums.size();
+
+       for(int i =0; i < n-1; i++){
+        if(i > maxInd)
+            return false;
+        int currmax = i + nums[i];
+        maxInd = max(maxInd, currmax);
+        cout <<maxInd <<" " << currmax <<endl;
+       }
+       if(maxInd >= n-1)
+        return true;
+        else 
+            return false;
+    }
+};
