521a60502cf6f74a3882456b838db52aea33f3b0
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-06-21 02:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (18.67%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,39 @@
-    class Solution {
-    public:
-        vector<int> searchRange(vector<int>& nums, int target) {
-            int n = nums.size();
-            int low = 0;
-            int high = n-1;
-            int ansL =-1, ansR = -1;
-            
-            while (low <= high){
-                int mid = low+ (high - low) /2; 
-            if(nums[mid]== target){
-                ansL = mid;
-                high = mid -1;
-            }            
-            else if(nums [mid] > target){
-                high = mid-1;
-            }
-            else {
-                low = mid+1;
-            }
-        }
-            low = 0;
-            high = n-1;
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
 
-            while (low <= high){
-                int mid = low+ (high - low) /2;
+        int firstocc = -1;
+        int lastocc = -1;
+
+        while(low <= high){
+            int mid = (high + low) /2;
+
             if(nums[mid] == target){
-                ansR = mid;
-                low = mid+1;
-            } 
-            else if(nums [mid] < target){          
-                low = mid+1;
+                    firstocc = mid;
+                    high = mid -1;
             }
-            else {
-                    high = mid-1;
-            }
+            else if(nums[mid] > target)
+                high = mid -1;
+            else 
+                low = mid + 1;              
         }
+        low = 0;
+        high = n-1;
+        while(low <= high){
+           int mid = (low + high) /2;
 
-   
-    
-         return {ansL,ansR};
-            }  
-
+            if(nums[mid] == target){
+                lastocc = mid;
+                low = mid + 1;
+            }
+            else if (nums[mid] > target)
+                high = mid -1;
+            else 
+                low = mid +1;
+        }
+        return {firstocc, lastocc};
+    }
 };
