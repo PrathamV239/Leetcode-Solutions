54141a9ab5d1cd0f5f404d824450d4d39fc34a88
Pratham Vidhani <prathamvidhani239@gmail.com>
2026-06-20 21:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (42.38%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,19 +4,19 @@ public:
         int n = nums.size();
         int low = 0;
         int high = n-1;
-        int ans = n;
-
+   
         while(low <= high){
-            int mid = (low + high) /2;
+            int mid = (high + low) /2;
 
-            if(nums[mid] >= target){
-                ans = mid;
-                high = mid-1;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target){
+                // result = mid;
+                high = mid -1;
             }
-            else
+            else 
                 low = mid +1;
         }
-        return ans;
-
+        return low;
     }
 };
